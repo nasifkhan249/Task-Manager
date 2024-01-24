f8cffc2c753fa5fb02833df1cd848b96511c85c2
Nasif Mustufa Khan <97841869+nasifkhan249@users.noreply.github.com>
2024-01-24 22:56:52 +0600

    updated

---
+++ client/src/components/Cancled/Cancled.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cancled = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Cancled;
---
+++ client/src/pages/AccountRecover/VerifyOTPPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const VerifyOTPPage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default VerifyOTPPage;
